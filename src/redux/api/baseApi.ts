7f9c95cb809c3a984a1d8de09883c32d0c651a7e
Tanvir Rudro <rudro987@gmail.com>
2024-09-14 22:05:27 +0600

    set new access token after it expires using refresh token

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -1,5 +1,6 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { RootState } from "../store";
+import { setUser } from "../features/auth/authSlice";
 
 const baseQuery = fetchBaseQuery({ 
   baseUrl: "http://localhost:5000/api/v1" ,
@@ -11,11 +12,36 @@ const baseQuery = fetchBaseQuery({
     }
     return headers;
   },
-  // Include cookies in the request
 });
+
+const baseQueryWithRefreshToken = async (args, api, extraOptions) => {
+
+  let result = await baseQuery(args, api, extraOptions);
+
+  if(result.error?.status === 401){
+    //* Send refresh token
+    const res = await fetch('http://localhost:5000/api/v1/auth/refresh-token', {
+      method: "POST",
+      credentials: "include",
+    });
+
+    const data = await res.json();
+
+    const user = (api.getState() as RootState).auth.user;
+
+    api.dispatch(setUser({
+      user: user,
+      token: data.data.accessToken
+    }))
+    
+    result = await baseQuery(args, api, extraOptions);
+  }
+
+  return result;
+};
 
 export const baseApi = createApi({
   reducerPath: "baseApi",
-  baseQuery: baseQuery,
+  baseQuery: baseQueryWithRefreshToken,
   endpoints: () => ({}),
 });
